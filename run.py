0fe817aa16212d84ce8fd511e5164f55ee02abec
NoobJsPerson <itsmeac101@gmail.com>
2021-07-27 13:44:44 +0100

    tweaks run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import asyncio
 async def get_new_runs(bot) -> None:
     async with session.get('https://www.speedrun.com/api/v1/runs?status=verified&orderby=verify-date&direction=desc') as response:
-        bot.logger.info("in get_new_runs")
-        bot.logger.info(response)
+    	runsdata = response.json()
+        '''TODO handle new runs'''
     await asyncio.sleep(300)
     await get_new_runs(bot)
   
