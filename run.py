91a4c82dd7a86ad6bf91db38666d6361c8b4b93d
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2021-07-28 23:49:17 +0200

    formatting and fix error

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import asyncio
+
 async def get_new_runs(bot) -> None:
-    async with session.get('https://www.speedrun.com/api/v1/runs?status=verified&orderby=verify-date&direction=desc') as response:
-    	runsdata = response.json()
-        '''TODO handle new runs'''
+    async with bot.session.get('https://www.speedrun.com/api/v1/runs?status=verified&orderby=verify-date&direction=desc') as response:
+        runsdata = response.json()
+        # TODO handle new runs
+
     await asyncio.sleep(300)
     await get_new_runs(bot)
-  
-  
